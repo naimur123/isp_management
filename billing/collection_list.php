c04582fe2acb1180ac850a5440d0bddf0dbcad2a
Naimur Rahman <17-34549-2@student.aiub.edu>
2026-09-06 15:09:02 +0600

    collection amount field issue fixed

--- billing/collection_list.php
+++ billing/collection_list.php
@@ -152,7 +152,7 @@ require_once __DIR__ . '/../includes/header.php';
               <!-- Toggle Button -->
                <?php 
                  $onclickAction = 'onclick="toggleInlineForm(this)"';
-                 if($r['collection_status'] === 'Paid' && !empty($r['collection_amount']) && !empty($r['bank_id'])){
+                 if($r['collection_status'] === 'Paid' && (!empty($r['collection_amount']) || (float)($r['collection_amount'] != 0)) && !empty($r['bank_id'])){
                     $onclickAction = "";
                  } 
                ?>
@@ -171,7 +171,7 @@ require_once __DIR__ . '/../includes/header.php';
                   <input type="hidden" name="current_status" value="<?= e($r['collection_status']) ?>">
             
                   <!-- Collection Amount -->
-                  <?php if(empty($r['collection_amount'])): ?>
+                  <?php if(empty($r['collection_amount']) || (float)$r['collection_amount'] == 0): ?>
                   <div class="mb-2">
                     <label class="form-label form-label-sm fw-bold mb-1">Collection Amount</label>
                     <input type="number" name="collection_amount" class="form-control form-control-sm">
